790af16efc511b62ea0cb1c172c2fd1972124590
MuhammadRizwan-29 <engrrizwanjamali@gmail.com>
2024-03-27 00:09:24 +0500

    Pricing: Toggle value functionality added

--- app/script.js
+++ app/script.js
@@ -1,43 +1,29 @@
-// const toggleButton = document.querySelector("#toggle--btn");
-// const monthlySpan = document.querySelector(".monthly");
-// const yearlySpan = document.querySelector(".yearly");
-
-// // toggleButton.addEventListener("change", () => {
-// //   if (toggleButton.checked) {
-// //     // monthlySpan.classList.add("selected");
-// //     // yearlySpan.classList.remove("selected");
-// //     monthlySpan.classList.contains("selected")
-// //       ? monthlySpan.classList.remove("selected")
-// //       : monthlySpan.classList.add("selected");
-// //   } else {
-// //     yearlySpan.classList.contains("selected")
-// //       ? monthlySpan.classList.remove("selected")
-// //       : monthlySpan.classList.add("selected");
-// //   }
-// // });
-// toggleButton.addEventListener("change", () => {
-//   if (toggleButton.checked) {
-//     monthlySpan.classList.add("selected");
-//     yearlySpan.classList.remove("selected");
-//   } else {
-//     yearlySpan.classList.add("selected");
-//     monthlySpan.classList.remove("selected");
-//   }
-// });
-// console.log(toggleButton);
-
 const toggleButton = document.getElementById("toggle--btn");
 const monthlyLabel = document.querySelector(".monthly");
 const annuallyLabel = document.querySelector(".annually");
+/* Pricing Values Selector*/
+const basicPlan = document.querySelector(".basic--plan .price--col h1");
+const proPlan = document.querySelector(".pro--plan .price--col h1");
+const businessPlan = document.querySelector(".business--plan .price--col h1");
+const priceTag = document.querySelectorAll(".plan--container .price--col p");
+
+const priceSet = function (basic, pro, business, tag) {
+  basicPlan.innerHTML = `${basic}.00`;
+  proPlan.innerHTML = `${pro}.00`;
+  businessPlan.innerHTML = `${business}.00`;
+  priceTag.textContent = `per ${tag}`;
+};
 
 toggleButton.addEventListener("change", () => {
   if (toggleButton.checked) {
     monthlyLabel.classList.remove("selected");
     annuallyLabel.classList.add("selected");
     // Update prices or values for annually here
+    priceSet(190.0, 390.0, 990.0, "month");
   } else {
     annuallyLabel.classList.remove("selected");
     monthlyLabel.classList.add("selected");
     // Update prices or values for monthly here
+    priceSet(390.0, 890.0, 1290.0, "year");
   }
 });
